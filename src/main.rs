539587663501b61a9e404c49e0a37478f59ce6e5
Hassan Jahwar <hassanjawhar@protonmail.com>
2023-04-13 03:22:39 +0300

    Code cleanup + added hyper + rustls

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-pub mod ws;
+pub mod modules;
 
-use crate::ws::conn::connection;
+use crate::modules::{conn::connection, https::run_server};
+use dotenv::dotenv;
 use ethers::prelude::*;
+use std::env;
 use tokio::task::JoinHandle;
 
 #[derive(Debug)]
@@ -11,7 +13,12 @@ pub struct CustomErr {
 
 #[tokio::main]
 async fn main() -> Result<(), CustomErr> {
-    let connection = connection().await;
+    dotenv().ok();
+    let node_ws_eth = env::var("WSS_NODE_ETH").expect("WSS Node endpoint is missing");
+    let certificate_path = env::var("CERTIFICATE_PATH").expect("Certificate path is missing");
+    let key_path = env::var("KEY_PATH").expect("Key path is missing");
+    let port = env::var("HTTPS_PORT").expect("Https port is missing");
+    let connection = connection(node_ws_eth.as_str()).await;
 
     let connection1 = connection.clone();
     let connection2 = connection.clone();
@@ -28,6 +35,15 @@ async fn main() -> Result<(), CustomErr> {
         let mut txs_stream = connection2.subscribe_pending_txs().await.unwrap();
         while let Some(tx) = txs_stream.next().await {
             println!("Transaction hash: {:?}", tx);
+        }
+    }));
+
+    thread_handles.push(tokio::spawn(async move {
+        if let Err(e) =
+            run_server(certificate_path.as_str(), key_path.as_str(), port.as_str()).await
+        {
+            eprintln!("FAILED: {}", e);
+            std::process::exit(1);
         }
     }));
 
--- src/modules/conn.rs
+++ src/modules/conn.rs
@@ -2,8 +2,7 @@ use std::time::Duration;
 
 use ethers::prelude::*;
 
-pub async fn connection() -> Provider<Ws> {
-    let endpoint = "";
+pub async fn connection(endpoint: &str) -> Provider<Ws> {
     let url = String::from(endpoint);
     let ws = Ws::connect(url).await.unwrap();
     let provider = Provider::new(ws).interval(Duration::from_millis(2000));
